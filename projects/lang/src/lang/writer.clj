f5c29f80a1b8f8b5f5c7b9f640519b13d6eba0e2
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-09-11 23:20:24 -0500

    change version header

--- projects/lang/src/lang/writer.clj
+++ projects/lang/src/lang/writer.clj
@@ -3,9 +3,9 @@
             [lang.bytes :refer [string->bytes]]))
 
 ; version 2.0
-(def current-version (byte-array [(byte 50)
-                                  (byte 48)
-                                  (byte 48)]))
+(def current-version (byte-array [(byte 127)
+                                  (byte 82)
+                                  (byte 50)]))
 (defn version [writer] 
   (do (.write writer current-version)))
 

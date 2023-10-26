38139842348d89d1251410d74c6dc28bbabdcb17
jacob <jacoobes@sern.dev>
2023-10-26 18:27:05 -0500

    tests, reformat

--- projects/rutile/project.clj
+++ projects/rutile/project.clj
@@ -1,9 +1,9 @@
 (defproject rutile "HEAD"
   :description "frontend language for kongoc written in clojure"
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [instaparse "1.4.12"]
-                 [org.clojure/tools.cli "1.0.219"]]
-  :plugins [[cider/cider-nrepl "0.28.1"]]
-  :global-vars {*warn-on-reflection* true}
-  :source-paths ["src" "src/main/clojure"]
-  :main lang.src.main)
+                 [instaparse "1.4.12"]]
+  :plugins [[ cider/cider-nrepl "0.22.1" :exclusions [nrepl] ]]
+  :repl { 
+    :dependencies [[org.clojure/tools.nrepl "0.2.13"]] 
+  }
+  :main rutile.main)
